8b0071ee343b4b8b9bdabdd2c0ad25dd8dd5260b
syed1255 <99645911+syed1255@users.noreply.github.com>
2022-06-11 22:34:56 +0530

    Update MealItem.js

--- src/Components/MealItem.js
+++ src/Components/MealItem.js
@@ -1,23 +1,23 @@
 import React from "react";
 //import './Components/style.css';
-import './Style.css';
-const Mealitem = (getMeal) => {
+import './style.css';
+const Mealitem=(getMeal)=>{
     console.log(getMeal.data)
-    return (
+    return(
         <>
-            <div className="card">
-                <img src={getMeal.data.strMealThumb} alt="meal" />
-                <div cSSSlassName="info">
-                    <h2>{getMeal.data.strMeal}</h2>
-                    <p>{getMeal.data.strArea} food</p>
-                </div>
-                <div className="recipe">
-                    <h2>Recipe</h2>
-                    <p>{getMeal.data.strInstructions}</p>
-                    <img src={getMeal.data.strMealThumb} />
-                    <a href={getMeal.data.strSource}>Watch video</a>
-                </div>
-            </div>
+             <div className="card">
+                        <img src={getMeal.data.strMealThumb} alt="meal"/>
+                        <div className ="info">
+                        <h2>{getMeal.data.strMeal}</h2>
+                        <p>{getMeal.data.strArea} food</p>
+                        </div>
+                        <div className ="recipe">
+                            <h2>Recipe</h2>
+                            <p>{getMeal.data.strInstructions}</p>
+                            <img src={getMeal.data.strMealThumb}/>
+                            <a href={getMeal.data.strSource}>Watch video</a>
+                        </div>
+            </div>  
         </>
     )
 }
