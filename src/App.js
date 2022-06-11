2166e426c89ea4556956466b21cdff15080baa69
unknown <rashidali3272@gmail.com>
2022-06-11 22:07:31 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import './Components/Style.css';
+import Meal from './Components/Meal.js';
+function App() {
+  return(
+      <Meal/>
+  )
+  
+}
+
+export default App;
